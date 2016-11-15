f385905315dfed3e6d2cfb97e0f344b8174ad7c4
hoduc <duc_ho90@yahoo.com>
2016-11-15 00:02:09 -0800

    lookup event on <ENTER> and <BUTTON> pressed

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from Tkinter import *
 
+ENTER_EVENT_ID = "<Return>"
+MOUSE1_EVENT_ID = "<Button-1>"
+
+def lookup(event):
+    print "event:lookup:", event.widget
+    print "looking up:", lu_input.get(), "...."
+    
 root = Tk()
 root.geometry("250x150+300+300")
-app = Frame(root)
-app.parent = root
-app.parent.title("Lookup")
-app.pack(fill=BOTH, expand=1)
+root.title("Lookup")
 
-disp_frame = Frame(app)
+#displaying the result of look up
+disp_frame = Frame(root)
 disp_frame.pack(fill=BOTH, anchor=N, expand=True)
-disp_entry = Entry(disp_frame, state='disabled')
+disp_entry = Entry(disp_frame, state=DISABLED)
 disp_entry.pack(fill=BOTH, padx=5,pady=5, expand=True)
 
-lu_frame = Frame(app)
+#input to the lookup
+lu_frame = Frame(root, name="lookup_frame")
 lu_frame.pack(fill=X, anchor=S )
 lu_label = Label(lu_frame, text="Enter:")
 lu_label.pack(side=LEFT, padx=5, pady=5)
-lu_input = Entry(lu_frame)
+lu_button = Button(lu_frame, text="Look up!!!", name="lookup_button")
+lu_button.pack(side=RIGHT, padx=5, pady=5)
+lu_button.bind(MOUSE1_EVENT_ID, lookup)
+lu_input = Entry(lu_frame, name="lookup_input")
 lu_input.pack(fill=X, padx=5, pady=5)
+lu_input.bind(ENTER_EVENT_ID, lookup)
 
 root.mainloop()
 
