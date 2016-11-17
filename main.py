e586013a9c141032884d4609a77868198cbcdcab
hoduc <duc_ho90@yahoo.com>
2016-11-17 00:20:27 -0800

    add unicode entry. need to figure out word wrap

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
 from tkinter import *
+from tkinter.scrolledtext import *
+
+import urllib
+import urllib.request
+import sys
 
 ENTER_EVENT_ID = "<Return>"
 MOUSE1_EVENT_ID = "<Button-1>"
 DICT = []
-STATES = [VI_CN,CN_VI]
-LOOKUP_TYPE = VI_CN
+STATES = ["VI_CN","CN_VI"]
+LOOKUP_TYPE = STATES[0]
 
 def lookup(event):
     print ("event:lookup:", event.widget)
     print ("looking up:", lu_input.get(), "....")
+    #print(DICT[0])
+    query = DICT[0][1]["sing_vi_cn"] + "=" +  urllib.parse.quote(lu_input.get().encode('utf-8'))
+    #disp_entry.insert(0,lu_input.get())
+    #print("query=",query)
+    #disp_entry.config(state=NORMAL)
+    #disp_entry.delete(0,END)
+    #disp_entry.insert(0, u"tôi")
+    #disp_entry.config(state="readonly")
 
+    with urllib.request.urlopen(query) as response:
+        text = response.read().decode("utf-8")
+        print(text)
+        disp_entry.config(state=NORMAL)
+        disp_entry.delete(0,END)
+        disp_entry.insert(0,text)
+        disp_entry.config(state=DISABLED)
+    
 def loadDictModule(filename="config"):
     dictname=""
     dictsource={}
     count = 0
     with open(filename, 'r') as f:
         for line in f:
-            if line[0] = "[":
+            if line[0] == "[":
                 if count:
                     DICT.append((dictname,dictsource))
                     dictsource = {}
                 dictname = line[:-1]
                 count += 1
             else:
-                l = line.split("=")
+                l = line.strip().split("=")
                 dictsource[l[0]] = l[1]             
-
+        DICT.append((dictname,dictsource))
+        
 loadDictModule()
 #GUI CODE
 root = Tk()
-root.geometry("250x150+300+300")
+root.tk.call('encoding', 'system', 'utf-8') #make decoding available
+root.geometry("300x250+300+300")
 root.title("Lookup")
 
 #displaying the result of look up
 disp_frame = Frame(root)
 disp_frame.pack(fill=BOTH, anchor=N, expand=True)
-disp_entry = Entry(disp_frame, state=DISABLED)
+disp_entry = Entry(disp_frame,state=DISABLED)
+disp_entry.focus_set()
 disp_entry.pack(fill=BOTH, padx=5,pady=5, expand=True)
 
 #input to the lookup
